23703137f88b4f11d8e2a41eed0e834182e5d5b5
Jacob B. Madsen <jacob@jbmadsen.com>
2020-02-28 13:47:56 +0100

    Added option to edit settings while program is running

--- src/git-watch.py
+++ src/git-watch.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import module.git as git
 import module.config as config
@@ -6,10 +7,21 @@ import module.toasts as toasts
 import PySimpleGUIWx as sg
 
 
+__git__ = None
+__root__ = None
+__freq__ = None
+
+
 def get_config_info():
     '''Get config information from config file'''
+    global __git__, __root__, __freq__
     git_path, root_path, poll_frequency = config.get_git_info() 
     root_path = os.path.normpath(root_path) 
+    if __git__ != git_path or __root__ != root_path or __freq__ != poll_frequency:
+        print("Config data hot-reloaded")
+        __git__ = git_path
+        __root__ = root_path
+        __freq__ = poll_frequency
     return git_path, root_path, poll_frequency
 
 
@@ -25,12 +37,21 @@ def display_git_information(git_path, root_path):
         #tray.ShowMessage(title="Git-watch reminder", message=message, time=7000)
 
 
+def open_settings():
+    '''Opens the config file in your favorite editor'''
+    didopen = config.open_config_file()
+    if not didopen:
+        toasts.toastMessage("Error", "Could not open file: {}".format(config.__config_path__))
+
+
 if __name__ == '__main__':
+    wd = os.getcwd()
+
     # Load/set parameters
     icon = os.path.join(os.getcwd(), 'src', 'assets', 'icon.ico')
-    menu_def = ['MENU', ['Settings', 'Exit']]
+    menu_def = ['MENU', ['Open settings', 'Exit']]
     git_path, root_path, poll_frequency = get_config_info()
-    
+
     # Start to system tray
     print("Starting git-watch (polling every {} seconds)".format(poll_frequency))
     tray = sg.SystemTray(menu=menu_def, filename=icon, tooltip="git-watch: running")
@@ -40,13 +61,24 @@ if __name__ == '__main__':
     try:
         i = 0
         while True:
+            # Reload config data
+            os.chdir(wd)
+            git_path, root_path, poll_frequency = get_config_info()
+    
             # Read input from tray icon
             event = tray.Read(timeout=1000)
 
             # Handle tray input if needed
-            if event == 'Exit': break
-            elif event == 'Settings': print("TODO") # TODO
-            else: pass #tray.ShowMessage('Event', '{}'.format(event))
+            if event == 'Exit': 
+                break
+            elif event == 'Open settings': 
+                # Reset click is apparently needed
+                tray.MenuItemChosen = sg.TIMEOUT_KEY
+                tray.TaskBarIcon.menu_item_chosen = sg.TIMEOUT_KEY
+                # Open settings if possible
+                open_settings()
+            else: 
+                pass #tray.ShowMessage('Event', '{}'.format(event))
 
             # Displays git information if needed
             if i >= poll_frequency:
--- src/module/config.py
+++ src/module/config.py
@@ -1,13 +1,75 @@
+import os
+import sys
+import subprocess
 import configparser
+
+
+__config_path__ = 'config.cfg'
+
+
+def find_config_file():
+    # get current directory 
+    wd = os.getcwd()
+    # Set current dir to working dir
+    os.chdir(wd)
+    # return filename if exists
+    if os.path.isfile(__config_path__):
+        return os.path.join(os.getcwd(), __config_path__)
+    # Set current dir to parent dir    
+    parent_dir = os.path.abspath(os.path.join(__config_path__, os.pardir))
+    os.chdir(parent_dir)
+    # return filename if exists
+    if os.path.isfile(__config_path__):
+        return os.path.join(os.getcwd(), __config_path__)
+    # Reset and return None
+    os.chdir(wd)
+    return None
 
 
 def get_git_info():
     '''Get git and repository root config information'''
-    cfg = configparser.ConfigParser()
-    cfg.read_file(open('./config.cfg'))
-
-    git = cfg.get('GIT','PATH')
-    root = cfg.get('SOURCE','PATH')
-    freq = int(cfg.get('APPLICATION','POLL_FREQUENCY'))
+    # Locate file
+    cfg = find_config_file()
+    if cfg is None:
+        raise FileNotFoundError("Config file: {} not found".format(__config_path__))
+    # Open file
+    with open(cfg) as fp:
+        config = configparser.ConfigParser()
+        config.readfp(fp)
     
+        git = config.get('GIT','PATH')
+        root = config.get('SOURCE','PATH')
+        freq = int(config.get('APPLICATION','POLL_FREQUENCY'))
+        
     return git, root, freq
+
+
+def open_config_file():
+    '''Opens the config file in your favorite editor, and returns True if file can be opened'''
+    # Locate file
+    cfg = find_config_file()
+    if cfg is None:
+        raise FileNotFoundError("Config file: {} not found".format(cfg))
+    # Open file
+    if sys.platform.startswith('darwin'):
+        try:
+            os.system('open "{0}"'.format(cfg))
+        except Exception:
+            return False
+    elif os.name == 'nt':
+        try:
+            os.startfile(cfg)
+        except OSError:
+            # [Error 22] No application is associated with the specified file for this operation
+            os.system("notepad.exe " + cfg)
+        except Exception:
+            return False
+    elif os.name == 'posix':
+        try:
+            os.system('xdg-open "{0}"'.format(cfg)) 
+        except Exception:
+            return False
+        # If all fails, try to use webbrowser
+        
+    return True
+        
